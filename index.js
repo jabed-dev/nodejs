e85ec98c07c7f39606a335991816a654a475b9b4
Jabed Hossain <59810760+jabed-dev@users.noreply.github.com>
2021-06-30 21:55:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http')
 
 http.createServer(function (require, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" })
-    response.end("<h1 style="text-align: center">Hello World!</h1>")
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('<h1 style="text-align: center">Hello World!</h1>')
 }).listen(process.env.PORT || 5050)
 
 console.log('Server is runing PORT 5050')
